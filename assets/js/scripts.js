900ec85564c0964f47349cad1a92dd8540a3b90f
hainers <59766738+hainers@users.noreply.github.com>
2021-03-09 15:47:02 +0200

    More work on front page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,21 @@ document.querySelector('.bcg-menu__toggle').addEventListener('click', function(e
         $menu.classList.add('bcg-menu--expanded');
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var glide = new Glide('.glide', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 5,
+        breakpoints: {
+            1024: {
+                perView: 3
+            },
+            600: {
+                perView: 2
+            }
+        }
+    })
+
+    glide.mount();
+});
